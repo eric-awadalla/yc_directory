866cd71f91020a30fbf6978c96330de0161635bd
Eric <ericgeorge4477@gmail.com>
2025-02-06 06:03:36 +0200

    remove imports

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,2 @@
-import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
-
 import { handlers } from "@/auth"; // Referring to the auth.ts we just created
 export const { GET, POST } = handlers;
