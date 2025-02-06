221093c0c1069e10366ffef3386a8c99882bc563
Eric <ericgeorge4477@gmail.com>
2025-02-06 05:54:41 +0200

    Add auth.ts configurations

--- auth.ts
+++ auth.ts
@@ -1,5 +1,19 @@
 import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    GitHubProvider({
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+  ],
+  pages: {
+    signIn: "/auth/login", // (Optional) Custom login page if needed
+  },
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      return url.startsWith(baseUrl) ? url : baseUrl;
+    },
+  },
 });
